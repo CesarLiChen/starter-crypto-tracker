203e39747008cd02152647edfbc9e201bae07a3d
Cesar Li (win10) <cesarli619@gmail.com>
2022-12-30 15:24:50 -0500

    Sets up price, coin endpoints

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const COIN_INFO_ENDPOINT = 
+	"https://min-api.cryptocompare.com/data/top/totalvol?limit=30&tsym=CAD";
+
+const PRICE_INFO_ENDPOINT =
+	"https://min-api.cryptocompare.com/data/pricemulti?tsyms=CAD&fsyms=";
+
+class CryptoApp {
+	constructor() {
+		this.coinDate = {};
+	}
+
+	// API - Application Program Interface
+	start() {
+
+	}
+}
