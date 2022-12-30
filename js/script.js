d92069572c1fc45abfbc2d73dfee8b0d319d432f
Cesar Li (win10) <cesarli619@gmail.com>
2022-12-30 15:34:34 -0500

    Sets up working HTTP requests

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// fsyms: from symbols
+// tsyms: to symbols
 const COIN_INFO_ENDPOINT = 
 	"https://min-api.cryptocompare.com/data/top/totalvol?limit=30&tsym=CAD";
 
@@ -6,11 +8,27 @@ const PRICE_INFO_ENDPOINT =
 
 class CryptoApp {
 	constructor() {
-		this.coinDate = {};
+		this.coinData = {};
 	}
 
 	// API - Application Program Interface
-	start() {
+	makeAPIcall(endPoint, callback) {
+		let xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				callback(JSON.parse(this.response));
+			}
+		}
+		xhttp.open("GET", endPoint, true);
+		xhttp.send();
+	}
 
+	start() {
+		this.makeAPIcall(COIN_INFO_ENDPOINT, coinData => {
+			console.log(coinData);
+		});
 	}
 }
+
+const crypto = new CryptoApp();
+crypto.start();
